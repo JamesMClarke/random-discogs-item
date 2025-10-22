a20ff78290fa39c8f9252f0a73820b4823741825
James Clarke <jamesmorganclarke@hotmail.co.uk>
2025-10-22 17:50:15 +0100

    Fixed cache and pagination

--- getData.go
+++ getData.go
@@ -110,10 +110,76 @@ func getFolders() []models.CollectionFolder {
 
 func getFolderItems(folderID int, folderName string) []models.Record {
 	// Get collection items from Discogs API using token and username
+	// TODO: Handle pagination
 	client := &http.Client{}
 	username := getUsername()
-	url := "https://api.discogs.com/users/" + username + "/collection/folders/" +
-		fmt.Sprintf("%d", folderID) + "/releases"
+	records := []models.Record{}
+	for page := 1; ; page++ {
+		url := "https://api.discogs.com/users/" + username + "/collection/folders/" +
+			fmt.Sprintf("%d", folderID) + "/releases?page=" + fmt.Sprintf("%d", page)
+		if debug {
+			fmt.Println("Fetching URL:", url)
+		}
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		addAuth(req)
+
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		// Read response body
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Parse JSON and return the collection items
+		var data map[string]interface{}
+		if err := json.Unmarshal(body, &data); err != nil {
+			log.Fatal(err)
+		}
+
+		if r, ok := data["releases"].([]interface{}); ok {
+			for _, item := range r {
+				itemBytes, err := json.Marshal(item)
+				if err != nil {
+					log.Fatal(err)
+				}
+				var record models.Record
+				if err := json.Unmarshal(itemBytes, &record); err != nil {
+					log.Fatal(err)
+				}
+				record.FolderName = folderName
+				records = append(records, record)
+			}
+		}
+
+		// Check for pagination
+		if pagination, ok := data["pagination"].(map[string]interface{}); ok {
+			if pages, ok := pagination["pages"].(float64); ok {
+				if debug {
+					fmt.Println("Folder:", folderName, "Page:", page, "of", int(pages))
+				}
+				if int(pages) <= page {
+					break
+				}
+			}
+		}
+	}
+	return records
+
+}
+
+func getLengthOfCollection() int {
+	// Get length of collection from Discogs API using token and username
+	client := &http.Client{}
+	username := getUsername()
+	url := "https://api.discogs.com/users/" + username + "/collection/folders/0"
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -132,26 +198,15 @@ func getFolderItems(folderID int, folderName string) []models.Record {
 		log.Fatal(err)
 	}
 
-	// Parse JSON and return the collection items
+	// Parse JSON and return the collection length
 	var data map[string]interface{}
 	if err := json.Unmarshal(body, &data); err != nil {
 		log.Fatal(err)
 	}
-
-	records := []models.Record{}
-	if r, ok := data["releases"].([]interface{}); ok {
-		for _, item := range r {
-			itemBytes, err := json.Marshal(item)
-			if err != nil {
-				log.Fatal(err)
-			}
-			var record models.Record
-			if err := json.Unmarshal(itemBytes, &record); err != nil {
-				log.Fatal(err)
-			}
-			record.FolderName = folderName
-			records = append(records, record)
-		}
+	if count, ok := data["count"].(float64); ok {
+		return int(count)
 	}
-	return records
+
+	log.Fatal("count not found in response")
+	return 0
 }
--- main.go
+++ main.go
@@ -1,29 +1,106 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 	"random-discogs-item/models"
 )
+
+// TODO: Add error handling
+// TODO: Add logic to only update cache if data has changed
+// TODO: Add command line flags for folder selection, etc.
+// TODO: Add command line flags for cache location, force refresh, etc.
+// TODO: Get random item from cached data depending on criteria
+var debug = true
 
 func main() {
 	auth := models.Auth{
 		Token:    getToken(),
 		Username: getUsername(),
 	}
-	fmt.Println("Token:", auth.Token)
-	fmt.Println("Username:", auth.Username)
+	if debug {
+		fmt.Println("Token:", auth.Token)
+		fmt.Println("Username:", auth.Username)
+	}
 
+	// Get length of collection
+	collectionLength := getLengthOfCollection()
+	cacheLength := getCacheLength()
+
+	if cacheLength != collectionLength {
+		fmt.Printf("Cached records is a different length (%d vs %d), updating cache...\n", cacheLength, collectionLength)
+		updateCache()
+	}
+}
+
+func cacheDir() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return home + "/.cache/random-discogs-item/"
+}
+
+func checkCacheDir() {
+	dir := cacheDir()
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func getCacheLength() int {
+	cacheFile := cacheDir() + "records_cache.json"
+	file, err := os.Open(cacheFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var records []models.Record
+	err = json.NewDecoder(file).Decode(&records)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return len(records)
+}
+
+func updateCache() {
 	records := []models.Record{}
 
 	folders := getFolders()
 	for _, folder := range folders {
+		if folder.Name == "All" {
+			continue
+		}
 		fmt.Printf("Folder ID: %d, Name: %s, Count: %d, Resource URL: %s\n",
 			folder.ID, folder.Name, folder.Count, folder.ResourceURL)
 		records = append(records, getFolderItems(folder.ID, folder.Name)...)
 	}
 
-	for _, record := range records {
-		fmt.Printf("Record ID: %d, Title: %s, Year: %d, Format: %s, Resource URL: %s, Folder Name: %s\n",
-			record.ID, record.BasicInformation.Title, record.BasicInformation.Year, record.BasicInformation.Formats, record.BasicInformation.ResourceURL, record.FolderName)
+	// for _, record := range records {
+	// 	fmt.Printf("Record ID: %d, Title: %s, Year: %d, Format: %s, Resource URL: %s, Folder Name: %s\n",
+	// 		record.ID, record.BasicInformation.Title, record.BasicInformation.Year, record.BasicInformation.Formats, record.BasicInformation.ResourceURL, record.FolderName)
+	// }
+
+	// Check and create cache directory
+	checkCacheDir()
+
+	// Save records to cache file
+	cacheFile := cacheDir() + "records_cache.json"
+	file, err := os.Create(cacheFile)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer file.Close()
+
+	err = json.NewEncoder(file).Encode(records)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Records cached to", cacheFile)
 }
